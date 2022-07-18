89f3f30da30807a9a9ca03b0b62b2196c5cdf7b9
Yelihi <Yelihi19@gmail.com>
2022-07-18 18:35:49 +0900

    Add: first commit. 초기 세팅 완료

---
+++ src/Router.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Router from './Router';
+import '../src/styles/common.scss';
+import '../src/styles/reset.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Router />);
