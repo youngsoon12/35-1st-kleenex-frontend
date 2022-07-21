1363533309af569322d3a6017ea4ab794c43d408
och.changhoon <och.changhoon@gmail.com>
2022-07-21 19:30:17 +0900

    [35기 오창훈] Modify: footer 컴포넌트 리뷰사항 반영

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,17 +9,14 @@ export default function Footer() {
       <section className="footerContainer">
         <article className="utilMenu">
           <div className="utilMenuContainer">
-            <div>
-              <Link to="">이용약관</Link>
-            </div>
-            <span>|</span>
-            <div>
-              <Link to="">개인정보처리방침</Link>
-            </div>
-            <span>|</span>
-            <div>
-              <Link to="">구매안전서비스 가입사실 확인</Link>
-            </div>
+            {UTILITIES.map(values => (
+              <>
+                <div key={values.id}>
+                  <Link to="">{values.util}</Link>
+                </div>
+                {values.id === UTILITIES.length ? '' : <span>|</span>}
+              </>
+            ))}
           </div>
         </article>
         <article className="contact">
@@ -31,15 +28,12 @@ export default function Footer() {
                 070-4914-9248
               </p>
               <div className="textCustomerCenter textDetails">
-                <p>
-                  평일 <span>09:00 ~ 18:00</span>
-                </p>
-                <p>
-                  점심 <span>12:00 ~ 13:00</span>
-                </p>
-                <p>
-                  휴무 <span>토/일/공휴일은 휴무</span>
-                </p>
+                {WORKING_TIME.map(values => (
+                  <p key={values.id}>
+                    {values.timeDivision}
+                    <span>{values.Specifictime}</span>
+                  </p>
+                ))}
               </div>
             </div>
 
@@ -57,43 +51,19 @@ export default function Footer() {
 
             <div className="companyInfo contactDivPreset">
               <div className="textCompanyInfo textDetails">
-                <p>
-                  상호<span>(주)학산</span>
-                </p>
-                <p>
-                  대표이사<span>김용덕</span>
-                </p>
-                <p>
-                  주소<span>서울시 강남구 테헤란로 427</span>
-                </p>
-                <p>
-                  통신판번호<span>2003-강원강릉-0085</span>
-                </p>
-                <p>
-                  계좌번호<span>신한은행 110-474-658716 (주)학산</span>
-                </p>
-                <p>
-                  사업자등록번호
-                  <span>
-                    226-81-28273
-                    <span className="spanBtn">[사업자정보확인]</span>
-                  </span>
-                </p>
-                <p>
-                  개인정보보호책임자<span>김첨지</span>
-                </p>
-                <p>
-                  원두도매문의<span>010-1234-5678</span>
-                </p>
-                <p>
-                  매장이용문의<span>02-1234-5678</span>
-                </p>
-                <p>
-                  레스토랑문의<span>010-8765-4321</span>
-                </p>
-                <p>
-                  이메일<span>info@kleenex.wecode.com</span>
-                </p>
+                {COMPANY_INFO.map(values => (
+                  <p key={values.id}>
+                    {values.title}
+                    <span>
+                      {values.value}
+                      {values.id === 6 ? (
+                        <span className="spanBtn">[사업자정보확인]</span>
+                      ) : (
+                        ''
+                      )}
+                    </span>
+                  </p>
+                ))}
               </div>
               <div className="copyright">
                 ⓒ2022 Team KLEENEX Wecode. ALL RIGHT RESERVED.
@@ -109,3 +79,94 @@ export default function Footer() {
     </footer>
   );
 }
+
+const UTILITIES = [
+  {
+    id: 1,
+    util: '이용약관',
+  },
+  {
+    id: 2,
+    util: '개인정보처리방침',
+  },
+  {
+    id: 3,
+    util: '구매안전서비스 가입사실 확인',
+  },
+];
+
+const WORKING_TIME = [
+  {
+    id: 1,
+    timeDivision: '평일',
+    Specifictime: '09:00 ~ 18:00',
+  },
+  {
+    id: 2,
+    timeDivision: '점심',
+    Specifictime: '09:00 ~ 18:00',
+  },
+  {
+    id: 3,
+    timeDivision: '휴무',
+    Specifictime: '토/일/공휴일은 휴무',
+  },
+];
+
+const COMPANY_INFO = [
+  {
+    id: 1,
+    title: '상호',
+    value: '(주)학산',
+  },
+  {
+    id: 2,
+    title: '대표이사',
+    value: '김용덕',
+  },
+  {
+    id: 3,
+    title: '주소',
+    value: '서울시 강남구 테헤란로 427',
+  },
+  {
+    id: 4,
+    title: '통신판번호',
+    value: '2003-강원강릉-0085',
+  },
+  {
+    id: 5,
+    title: '계좌번호',
+    value: '신한은행 110-474-658716 (주)학산',
+  },
+  {
+    id: 6,
+    title: '사업자등록번호',
+    value: '012-34-56789',
+  },
+  {
+    id: 7,
+    title: '개인정보보호책임자',
+    value: '김용덕',
+  },
+  {
+    id: 8,
+    title: '원두도매문의',
+    value: '010-8765-4321',
+  },
+  {
+    id: 9,
+    title: '매장이용문의',
+    value: '02-1234-5678',
+  },
+  {
+    id: 10,
+    title: '레스토랑문의',
+    value: '010-1234-5678',
+  },
+  {
+    id: 11,
+    title: '이메일',
+    value: 'info@kleenex.wecode.io',
+  },
+];
