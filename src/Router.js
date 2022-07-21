23b48b42bcaa271a826ca42b4cfeb36e6d79a046
youngsoon12 <enejwk789@gmail.com>
2022-07-22 08:41:02 +0900

    회원가입 커밋

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import Register from './pages/register/Register';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/register/Register.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './Register.scss';
+
+const Register = () => {
+  return (
+    <div className="Register">
+      <div className="nav" />
+      <div className="wrap">
+        <div className="container">
+          <div className="path">
+            <ul>
+              <li>회원가입</li>
+              <li>
+                HOME
+                <i class="bx bx-chevron-right" />{' '}
+              </li>
+            </ul>
+          </div>
+          <div className="titleArea">
+            <div className="logo">
+              <div className="logo1">J0IN</div>
+              <div className="logo2">회원가입</div>
+            </div>
+          </div>
+          <div className="inputTitle">기본정보 입력</div>
+          <form>
+            <table className="inputDataTable">
+              {inputData.map((data, index) => {
+                return (
+                  <tr key={index}>
+                    <th>
+                      <div>{data.th}</div>
+                    </th>
+                    <td>
+                      <input className="inputName" />
+                    </td>
+                  </tr>
+                );
+              })}
+            </table>
+          </form>
+        </div>
+      </div>
+      <div className="footer" />
+    </div>
+  );
+};
+
+const inputData = [
+  { th: '이름' },
+  { th: '아이디' },
+  { th: '비밀번호' },
+  { th: '비밀번호 확인' },
+  { th: '주소' },
+  { th: '휴대전화' },
+  { th: '이메일' },
+];
+
+export default Register;
