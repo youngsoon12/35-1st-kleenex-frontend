4f788900cec1cc5d88f29567ded17c8077681672
youngsoon12 <enejwk789@gmail.com>
2022-07-25 13:10:29 +0900

    컨플릭 해결 시도1

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
-import Register from './pages/register/Register';
 import Nav from './components/Nav/Nav';
 import Main from './pages/Main/Main';
-import Footer from './components/Footer/Footer';
-import RegisterSuccess from './pages/register/RegisterSuccess';
 
 const Router = () => {
   return (
@@ -13,11 +10,8 @@ const Router = () => {
       <Nav />
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route exact path="/register/success" element={<RegisterSuccess />} />
         <Route path="/main" element={<Main />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
