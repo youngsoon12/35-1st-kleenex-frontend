aac135e5b9840a7517e60d30066ebf2c03ce61a5
och.changhoon <och.changhoon@gmail.com>
2022-07-27 12:45:10 +0900

    Modify: ProductsList 컴포넌트 내부 변수 수정

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -11,8 +11,8 @@ export default function ProductList() {
   const location = useLocation();
 
   async function request(search) {
-    const res = await fetch(`http://10.58.3.145:8000/products${search}`);
-    // const res = await fetch('/data/productDataList.json');
+    // const res = await fetch(`http://10.58.3.145:8000/products${search}`);
+    const res = await fetch('/data/productDataList.json');
     const result = await res.json();
     setTotalItems(result.total);
     setProducts(result.shop_product_list);
