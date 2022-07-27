087acb658bd7b2f54787876bc28a72f8e0b49d6b
youngsoon12 <enejwk789@gmail.com>
2022-07-27 20:18:02 +0900

    재커밋

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './login.scss';
+import './Login.scss';
 
 function Login() {
   const [inputValue, setInputValue] = useState({
