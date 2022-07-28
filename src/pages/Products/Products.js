e070470d80f2da45bdc9c7938023a49a2cc30324
och.changhoon <och.changhoon@gmail.com>
2022-07-28 12:47:45 +0900

    wip

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,42 +1,14 @@
-import { useEffect, useState } from 'react';
-import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, Outlet } from 'react-router-dom';
 import { CATEGORY_MENU } from './ProductsData';
 import './Products.scss';
 
 export default function Product() {
   const [icon, iconChange] = useState('plus');
-  const [filtersList, setFiltersList] = useState([FILTER_DATA]);
-  const navigate = useNavigate();
 
   const iconHandler = () => {
     return icon === 'plus' ? iconChange('minus') : iconChange('plus');
   };
-
-  const filterHandler = e => {
-    e.preventDefault();
-    const { name } = e.target;
-
-    FILTER_DATA.map(element => {
-      if (element.filterName === name) {
-        element.isFilter === true
-          ? (element.isFilter = false)
-          : (element.isFilter = true);
-      }
-      setFiltersList(FILTER_DATA);
-    });
-  };
-
-  useEffect(() => {
-    let queryString = '';
-    filtersList.map(element => {
-      if (element.isFilter === true) {
-        queryString += `taste=${element.name}`;
-        console.log(queryString);
-      }
-    });
-  }, [filtersList]);
-
-  console.log(filtersList);
 
   return (
     <div className="product">
@@ -65,36 +37,9 @@ export default function Product() {
               </li>
             ))}
           </ul>
-          <div className="filterContainer">
-            {FILTER_DATA.map(taste => {
-              return (
-                <label key={taste.id}>
-                  <input
-                    type="checkbox"
-                    name={taste.filterName}
-                    onClick={e => filterHandler(e)}
-                  />
-                  {taste.filterName}
-                </label>
-              );
-            })}
-          </div>
         </div>
         <Outlet />
       </div>
     </div>
   );
 }
-
-const FILTER_DATA = [
-  { id: 1, filterName: 'Apple', isFilter: false },
-  { id: 2, filterName: 'Plum', isFilter: false },
-  { id: 3, filterName: 'Lime', isFilter: false },
-  { id: 4, filterName: 'Peach', isFilter: false },
-  { id: 5, filterName: 'Cool', isFilter: false },
-  { id: 6, filterName: 'Mango', isFilter: false },
-  { id: 7, filterName: 'Passion fruit', isFilter: false },
-  { id: 8, filterName: 'Grapefruit', isFilter: false },
-  { id: 9, filterName: 'Hibiscus', isFilter: false },
-  { id: 10, filterName: 'Floral', isFilter: false },
-];
